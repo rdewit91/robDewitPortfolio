afffe372a80c7039d056b1bd6ccc478b25068b05
rdewit91 <r.dewit91@outlook.com>
2023-04-13 14:16:53 -0400

    rough draft

--- rob-dewit-portfolio/src/pages/myEdu.js
+++ rob-dewit-portfolio/src/pages/myEdu.js
@@ -1,28 +1,47 @@
 import React from 'react'
-import styles from '@/styles/myEdu.module.css'
+import styles from '@/styles/myExpEdu.module.css'
+import Image from 'next/image'
+import fullStack from '../styles/assets/myExpEdu/fullStack.jpg'
+import brkPrt from '../styles/assets/myExpEdu/brockport.png'
 
 export default function myEdu() {
   return (
     <>
-    <div className={styles.myEduBox} >
-      <div className={styles.myEduHead} >
+    <div className={styles.myBox} >
+      <div className={styles.myHead} >
         <h1>WELCOME TO MY EDUCATION PAGE!!</h1>
         <p>Here are my most academic achievments</p>
       </div>
       <br></br>
       <br></br>
-      <div className={styles.myEduData} >
-        <h1>FullStack Academy</h1>
-
+      <div className={styles.myData} >
+        <div className={styles.myText} >
+          <h1>FullStack Academy</h1>
+          <p>Online Full Stack Web Development Immersive  Boot Camp</p>
+          <p>Date: Nov/2022 - Apr/2023</p>
+          <p>Degree Earned Certificate of Completion</p>
+          <p>Field of Study: JavaScript, HTML, CSS</p>
+        </div>
+        <Image src={fullStack} height="400" width="800"/>
       </div>
       <br></br>
       <br></br>
       <br></br>
-      <div className={styles.myEduData} >
-        <h1>State University of New York at Brockport</h1>
-
+      <div className={styles.myData} >
+        <div className={styles.myText} >
+          <h1>State University of New York at Brockport</h1>
+          <p>On-Site State University</p>
+          <p>Date: Aug/2014 - May/2018</p>
+          <p>Degree Earned: Bachelors of Science</p>
+          <p>Field of Study: Criminal Justice / Sociology</p>
+        </div>
+        <Image src={brkPrt} height="400" width="800"/>
+        
       </div>
     </div>
+    <div class='light x1'></div>
+    <div class='light x2'></div>
+    <div class='light x3'></div>
     <div class='light x4'></div>
     <div class='light x5'></div>
     <div class='light x6'></div>
--- rob-dewit-portfolio/src/pages/myExp.js
+++ rob-dewit-portfolio/src/pages/myExp.js
@@ -1,42 +1,70 @@
 import React from 'react'
-import styles from '@/styles/myExp.module.css'
+import styles from '@/styles/myExpEdu.module.css'
+import Image from 'next/image'
+import grda from '../styles/assets/myExpEdu/garda.jpg'
+import nxtGen from '../styles/assets/myExpEdu/nxtGen.jpg'
+import g4s from '../styles/assets/myExpEdu/g4s.png'
 
 export default function myExp() {
   return (
     <>
-    <div className={styles.myExpBox} >
-      <div className={styles.myExpHead} >
-        <h1>WELCOME TO MY EXPERIANCE PAGE!!!</h1>
+    <div className={styles.myBox} >
+      <div className={styles.myHead} >
+        <h2>WELCOME TO MY EXPERIANCE 2AGE!!!</h2>
         <p>Here are my most recent work experiances</p>
       </div>
       <br></br>
       <br></br>
-      <div className={styles.myExpData} >
-        <h1>Armed Messenger / Driver</h1>
-        <h2>Gardaworld</h2>
-
+      <div className={styles.myData} >
+        <div className={styles.myText} >
+          <h2>Position: Armed Messenger / Driver</h2>
+          <h3>Company: Gardaworld</h3>
+          <p>Date: Feb/2021 - Nov/2022</p>
+          <p>Location: Rochester, NY</p>
+          <p>Description:</p>
+          <p> -Trusted to operate an armored vehicle and firearm to transport large sums of currency from customer to a sorting center,</p>
+          <p> -Maintained a 99% customers serviced and reduce the amount of currency loss</p>
+        </div>
+        <Image src={grda}  height="400" width="400"/>
       </div>
       <br></br>
       <br></br>
-      <div className={styles.myExpData} >
-        <h1>Productions Specialist</h1>
-        <h2>NextGen Building Components</h2>
-
+      <div className={styles.myData} >
+        <div className={styles.myText} >
+          <h2>Position: Productions Specialist</h2>
+          <h3>Company: NextGen Building Components</h3>
+          <p>Location: Farmington, NY</p>
+          <p>Date: Aug/2020 - Feb/2021</p>
+          <p>Description:</p>
+          <p> -Coordinated and Cooperated with others to make designed cuts, complete assembly orders and safely ship out roof trusses.</p>
+          <p> -Increased production by 30% while maintaining a high standard of safety in a production environment</p>
+        </div>
+        <Image src={nxtGen} height="400" width="400"/>
       </div>
       <br></br>
       <br></br>
-      <div className={styles.myExpData} >
-        <h1>Customs & Protections Officer</h1>
-        <h2>G4S Secure Solutions</h2>
-
+      <div className={styles.myData} >
+        <div className={styles.myText} >
+          <h2>Position: Customs & Protections Off2cer</h2>
+          <h3>Company: G4S Secure S2lutions</h3>
+          <p>Location: Rochester, NY</p>
+          <p>Date: Oct/2018 - Sep/2020</p>
+          <p>Description:</p>
+          <p> -Patrolled and maintained a safe commuting environment in a highly populated area transit center</p>
+          <p> -Vigilantly and proficiently reacted to medical emergencies and high risk situations</p>
+        </div>
+        <Image src={g4s} height="400" width="400"/>
       </div> 
     </div>
+    <div class='light x1'></div>
+    <div class='light x2'></div>
+    <div class='light x3'></div>
     <div class='light x4'></div>
     <div class='light x5'></div>
     <div class='light x6'></div>
     <div class='light x7'></div>
     <div class='light x8'></div>
-    <div class='light x9'></div>  
+    <div class='light x9'></div> 
     </>
   )
 }
